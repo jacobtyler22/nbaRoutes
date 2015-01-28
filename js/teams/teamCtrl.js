022f05c2a1228a55bf0a3457bee5d5b02f5a2e32
Jacob Tyler <jacobtyler22@icloud.com>
2015-01-28 11:33:45 -0700

    Update

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,34 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
+	$scope.newGame = {};
+	$scope.showNewGameForm = false;
 
+	$scope.toggleNewGameForm = function(){
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+
+	if($routeParams.team === 'utahjazz'){
+		$scope.homeTeam = 'Utah Jazz';
+		$scope.logoPath = 'images/jazz-logo.png';
+	} else if($routeParams.team === 'losangeleslakers'){
+		$scope.homeTeam = 'Los Angeles Lakers';
+		$scope.logoPath = 'images/lakers-logo.png';
+	} else if($routeParams.team === 'miamiheat'){
+		$scope.homeTeam = 'Miami Heat';
+		$scope.logoPath = 'images/heat-logo.png';
+	}
+
+	$scope.submitGame = function(){
+		debugger;
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		teamService.addNewGame($scope.newGame).then(function(){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(data){
+				$scope.teamData = data;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			})
+		})
+	}
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -1,5 +1,34 @@
 var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
+	this.addNewGame = function(gameObj){
+		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+		if(parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)){
+			gameObj.won = true;
+		} else{
+			gameObj.won = false;
+		}
+		return $http.post(url, gameObj);
+	}
 
+	this.getTeamData = function(team){
+		var dfd = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http.get(url).then(function(data){
+			var results = data.data.results;
+			var wins = 0;
+			var losses = 0;
+			for(var i = 0; i < results.length; i++){
+				if(results[i].won){
+					wins++;
+				}else{
+					losses++;
+				}
+			}
+			results['wins'] = wins;
+			results['losses'] = losses;
+			dfd.resolve(results);
+		});
+		return dfd.promise;
+	}
 });
